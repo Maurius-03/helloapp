52aebad3e8dadefd32ab493fe6ba6b15d4dbfc2d
= <=>
2015-05-21 21:25:18 -0700

    First commit

--- hello.py
+++ hello.py
@@ -1,12 +1,15 @@
+#import Flask class from flask
 from flask import Flask
+#instantiate a class from Flask called app
 app = Flask(__name__)
 
 #route: an entry point for an app to return HTML content
 #'@' is a decorator: if user hits top-level directory ('/'), will execute fn
-@app.route('/')
+#If top-level directory used, function will handle request
 def hello_world():
     return 'Hello World!'
 
 #only runs if app is run from shell
+# if name of file is __main__ , being run from terminal .: start server
 if __name__ == '__main__':
     app.run()
